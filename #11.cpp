6b4d8eedb333bf248083581243558ac7bb57f4ad
Nikita Volkov <rtyuehe12321@yandex.ru>
2024-10-20 00:21:50 +0300

    Create #11.cpp

---
+++ #11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string sent = "Can you can a can as a canner can can a can?";
+    string n;
+    cout << "Введите слово: ";
+    cin >> n;
+    string s;
+    cout <<"Can you "<<n<<" a "<<n<<" as a cannar "<<n<<" "<<n<< " a "<<n<<"?";
+    return 0;
+}
